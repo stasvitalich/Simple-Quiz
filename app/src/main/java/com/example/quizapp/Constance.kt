4919e6ed8c84768bac789424aace6faf0b4c7242
stasvitalich <stasvitalich1994@gmail.com>
2023-02-19 15:06:11 +0600

    Another 30 questions were added to Constance.

--- app/src/main/java/com/example/quizapp/Constance.kt
+++ app/src/main/java/com/example/quizapp/Constance.kt
@@ -338,6 +338,335 @@ object Constance {
         )
         questionsList.add(question30)
 
+        val question31 = Question(
+            31, "Кто изображён на фотографии?",
+            R.drawable.baiden,
+            "Трамп",
+            "Картер",
+            "Байден",
+            "Трумэн",
+            3
+        )
+        questionsList.add(question31)
+
+        val question32 = Question(
+            32, "Кто изображён на фотографии?",
+            R.drawable.kuchma,
+            "Шрёдер",
+            "Чубайс",
+            "Кучма",
+            "Кравчук",
+            3
+        )
+        questionsList.add(question32)
+
+        val question33 = Question(
+            33, "Кто изображён на фотографии?",
+            R.drawable.pavelperviy,
+            "Наполеон",
+            "Людовик 14",
+            "Павел 1",
+            "Лжедмитрий 2",
+            3
+        )
+        questionsList.add(question33)
+
+        val question34 = Question(
+            34, "Кто изображён на фотографии?",
+            R.drawable.evgeniyprimakov,
+            "Примаков",
+            "Фрадков",
+            "Касьянов",
+            "Грызлов",
+            1
+        )
+        questionsList.add(question34)
+
+        val question35 = Question(
+            35, "Кто изображён на фотографии?",
+            R.drawable.tokaev,
+            "Кенжетаев",
+            "Сатпаев",
+            "Наарбаев",
+            "Токаев",
+            4
+        )
+        questionsList.add(question35)
+
+        val question36 = Question(
+            36, "Кто изображён на фотографии?",
+            R.drawable.sunak,
+            "Сингх",
+            "Сунак",
+            "Моди",
+            "Нетаньяху",
+            2
+        )
+        questionsList.add(question36)
+
+        val question37 = Question(
+            37, "Кто изображён на фотографии?",
+            R.drawable.mandela,
+            "Кинг",
+            "Обама",
+            "Мандела",
+            "Лумумба",
+            3
+        )
+        questionsList.add(question37)
+
+        val question38 = Question(
+            38, "Кто изображён на фотографии?",
+            R.drawable.surikov,
+            "Суриков",
+            "Грызлов",
+            "Байден",
+            "Калашников",
+            1
+        )
+        questionsList.add(question38)
+
+        val question39 = Question(
+            39, "Кто изображён на фотографии?",
+            R.drawable.navalny,
+            "Иванов",
+            "Шольц",
+            "Ходорковский",
+            "Навальный",
+            4
+        )
+        questionsList.add(question39)
+
+        val question40 = Question(
+            40, "Кто изображён на фотографии?",
+            R.drawable.shreder,
+            "Шрёдер",
+            "Шольц",
+            "Берлускони",
+            "Аденауэр",
+            1
+        )
+        questionsList.add(question40)
+
+        val question41 = Question(
+            41, "Кто изображён на фотографии?",
+            R.drawable.bushmladshiy,
+            "Буш младший",
+            "Эйзенхауэр",
+            "Гувер",
+            "Пирс",
+            1
+        )
+        questionsList.add(question41)
+
+        val question42 = Question(
+            42, "Кто изображён на фотографии?",
+            R.drawable.hrushev,
+            "Берия",
+            "Ленин",
+            "Хрущёв",
+            "Брежнев",
+            3
+        )
+        questionsList.add(question42)
+
+        val question43 = Question(
+            43, "Кто изображён на фотографии?",
+            R.drawable.karlin,
+            "Медведев",
+            "Карлин",
+            "Минниханов",
+            "Фрадков",
+            2
+        )
+        questionsList.add(question43)
+
+        val question44 = Question(
+            44, "Кто изображён на фотографии?",
+            R.drawable.grudinin,
+            "Соловьёв",
+            "Грудинин",
+            "Кравчук",
+            "Берия",
+            2
+        )
+        questionsList.add(question44)
+
+        val question45 = Question(
+            45, "Кто изображён на фотографии?",
+            R.drawable.nikolayvtoroy,
+            "Георг 5",
+            "Александр 1",
+            "Николай 1",
+            "Николай 2",
+            4
+        )
+        questionsList.add(question45)
+
+        val question46 = Question(
+            46, "Кто изображён на фотографии?",
+            R.drawable.tetcher,
+            "Елизавета 2",
+            "Принцесса Диана",
+            "Тэтчер",
+            "Меркель",
+            3
+        )
+        questionsList.add(question46)
+
+        val question47 = Question(
+            47, "Кто изображён на фотографии?",
+            R.drawable.lukashenko,
+            "Лукашенко",
+            "Медведчук",
+            "Шойгу",
+            "Мантуров",
+            1
+        )
+        questionsList.add(question47)
+
+        val question48 = Question(
+            48, "Кто изображён на фотографии?",
+            R.drawable.kennedy,
+            "Трумэн",
+            "Кеннеди",
+            "Никсон",
+            "Картер",
+            2
+        )
+        questionsList.add(question48)
+
+        val question49 = Question(
+            49, "Кто изображён на фотографии?",
+            R.drawable.gorbachev,
+            "Титов",
+            "Сахаров",
+            "Горбачёв",
+            "Ельцин",
+            3
+        )
+        questionsList.add(question49)
+
+        val question50 = Question(
+            50, "Кто изображён на фотографии?",
+            R.drawable.gorbachev,
+            "Кисида",
+            "Ким Чен Ын",
+            "Юн Сок Ёль",
+            "Си Цзиньпин",
+            4
+        )
+        questionsList.add(question50)
+
+        val question51 = Question(
+            51, "Кто изображён на фотографии?",
+            R.drawable.pyotrtretiy,
+            "Фёдор 3",
+            "Трубецкой",
+            "Вильгельм 3",
+            "Пётр 3",
+            4
+        )
+        questionsList.add(question51)
+
+        val question52 = Question(
+            52, "Кто изображён на фотографии?",
+            R.drawable.klinton,
+            "Клинтон",
+            "Вильсон",
+            "Гаррисон",
+            "Гарфилд",
+            1
+        )
+        questionsList.add(question52)
+
+        val question53 = Question(
+            53, "Кто изображён на фотографии?",
+            R.drawable.sholc,
+            "Брандт",
+            "Шольц",
+            "Шмидт",
+            "Коль",
+            2
+        )
+        questionsList.add(question53)
+
+        val question54 = Question(
+            54, "Кто изображён на фотографии?",
+            R.drawable.mishustin,
+            "Мишустин",
+            "Кириенко",
+            "Степашин",
+            "Зубков",
+            1
+        )
+        questionsList.add(question54)
+
+        val question55 = Question(
+            55, "Кто изображён на фотографии?",
+            R.drawable.brezhnev,
+            "Рыков",
+            "Троцкий",
+            "Брежнев",
+            "Ежов",
+            3
+        )
+        questionsList.add(question55)
+
+        val question56 = Question(
+            56, "Кто изображён на фотографии?",
+            R.drawable.yushenko,
+            "Масол",
+            "Яценюк",
+            "Ющенко",
+            "Тимошенко",
+            3
+        )
+        questionsList.add(question56)
+
+        val question57 = Question(
+            57, "Кто изображён на фотографии?",
+            R.drawable.kasianov,
+            "Касьянов",
+            "Каспаров",
+            "Ходорковский",
+            "Галицкий",
+            1
+        )
+        questionsList.add(question57)
+
+        val question58 = Question(
+            58, "Кто изображён на фотографии?",
+            R.drawable.ekaterinavtoraya,
+            "Мария Фёдоровна",
+            "Екатерина 1",
+            "Александра Фёдоровна",
+            "Екатерина 2",
+            4
+        )
+        questionsList.add(question58)
+
+        val question59 = Question(
+            59, "Кто изображён на фотографии?",
+            R.drawable.kravchuk,
+            "Кравчук",
+            "Черномырдин",
+            "Степашин",
+            "Белоусов",
+            1
+        )
+        questionsList.add(question59)
+
+        val question60 = Question(
+            60, "Кто изображён на фотографии?",
+            R.drawable.chernenko,
+            "Титов",
+            "Черненко",
+            "Сахаров",
+            "Ельцин",
+            2
+        )
+        questionsList.add(question60)
 
         return questionsList
     }
