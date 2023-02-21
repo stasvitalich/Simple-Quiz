9d1cefd58d183cab2361da909296d7e0a3399972
stasvitalich <stasvitalich1994@gmail.com>
2023-02-21 16:56:43 +0600

    ui of the last activity was updated. confetti library was added.

--- app/src/main/java/com/example/quizapp/ResultActivity.kt
+++ app/src/main/java/com/example/quizapp/ResultActivity.kt
@@ -1,10 +1,15 @@
 package com.example.quizapp
 
+
 import android.app.Instrumentation.ActivityResult
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 import com.example.quizapp.databinding.ActivityResultBinding
+import nl.dionsegijn.konfetti.core.Party
+import nl.dionsegijn.konfetti.core.Position
+import nl.dionsegijn.konfetti.core.emitter.Emitter
+import java.util.concurrent.TimeUnit
 
 class ResultActivity : AppCompatActivity() {
     lateinit var binding: ActivityResultBinding
@@ -13,6 +18,24 @@ class ResultActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityResultBinding.inflate(layoutInflater)
-        setContentView(R.layout.activity_result2)
+        setContentView(binding.root)
+
+        initUI()
+
+    }
+
+    private fun initUI() {
+        val party = Party(
+            speed = 0f,
+            maxSpeed = 30f,
+            damping = 0.9f,
+            spread = 360,
+            colors = listOf(0xfce18a, 0xff726d, 0xf4306d, 0xb48def),
+            emitter = Emitter(duration = 100, TimeUnit.MILLISECONDS).max(100),
+            position = Position.Relative(0.5, 0.3)
+        )
+
+        binding.konfettiView.start(party)
+
     }
 }
